c56e075793dde9bfbc484a165a6872cfb9e1ae9e
Michael McEniry [UAH] <mcenirm@uah.edu>
2019-06-02 17:04:32 -0500

    Use deploy instead of create stack

---
+++ sandbox-deploy-stack.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eu
+
+for missing_environment_variable in KeyName ImageId
+do
+  : ${!missing_environment_variable:?$missing_environment_variable}
+done
+
+stack_name=dhis2-sandbox
+template_file=stack.$stack_name.json
+
+python3 sandbox_generate_template.py | jq -S . > "$template_file"
+
+(
+    set -x
+    aws cloudformation deploy \
+        --stack-name "$stack_name" \
+        --template-file "$template_file" \
+        --parameter-overrides \
+            KeyName="$KeyName" \
+            ImageId="$ImageId"
+)
